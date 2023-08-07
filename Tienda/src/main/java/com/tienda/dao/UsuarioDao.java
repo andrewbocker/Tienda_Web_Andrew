2742d94d169be2ae067937fcb27376de0129f99a
andrewbocker <andrew.bocker26@gmail.com>
2023-08-06 22:02:02 -0700

    commit semana 11

--- Tienda/src/main/java/com/tienda/dao/UsuarioDao.java
+++ Tienda/src/main/java/com/tienda/dao/UsuarioDao.java
@@ -11,6 +11,12 @@ public interface UsuarioDao extends JpaRepository<Usuario, Long> {
 
     Usuario findByUsername(String username);
 
+    Usuario findByUsernameAndPassword(String username, String Password);
+
+    Usuario findByUsernameOrCorreo(String username, String correo);
+
+    boolean existsByUsernameOrCorreo(String username, String correo);
+
 }
 
 
